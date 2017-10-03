9fc04899e612fc4e2cc24d579646534033db801b
Talovic <dzevad88@gmail.com>
2017-10-03 21:28:31 +0200

    Working on creating new user record in database

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a record with the given profile ID
+        } else {
+          // we don't have a user record with this ID, make a new record
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
