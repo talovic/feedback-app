9e62f8479b86fb196e16bba7d4e2ecc4064a5a3b
Talovic <dzevad88@gmail.com>
2017-10-04 22:24:59 +0200

    Adding cookie authentication

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -16,9 +26,12 @@ passport.use(
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           //we already have a record with the given profile ID
+          done(null, existingUser);
         } else {
           // we don't have a user record with this ID, make a new record
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
       });
     }
