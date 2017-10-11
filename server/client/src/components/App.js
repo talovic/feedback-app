64b2f54d8ca38a746a1dfa51af60f2a26485f202
Talovic <dzevad88@gmail.com>
2017-10-11 21:03:46 +0200

    Initial header component setup

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -10,9 +10,11 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div>
+        <div className="container">
+          <Header />
           <Route exact path="/" component={Landing} />
-          <Route path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
