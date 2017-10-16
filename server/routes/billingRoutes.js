6061d9511cbba63c3dfa584a05110076d9772124
Talovic <dzevad88@gmail.com>
2017-10-16 22:33:09 +0200

    Working on payments..

---
+++ server/routes/billingRoutes.js
@@ -0,0 +1,8 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
+module.exports = app => {
+  app.post('/api/stripe', (req, res) => {
+    console.log(req.body);
+  });
+};
