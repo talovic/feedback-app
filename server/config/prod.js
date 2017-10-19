6638c877f71f00853692fd959934f66ec415f2d1
Talovic <dzevad88@gmail.com>
2017-10-19 20:48:38 +0200

    fixed typo

--- server/config/prod.js
+++ server/config/prod.js
@@ -4,6 +4,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: proces.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: proces.env.STRIPE_SECRET_KEY
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
