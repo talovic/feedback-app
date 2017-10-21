63caec79b85518217f95f4d1d388b21eeae77575
Talovic <dzevad88@gmail.com>
2017-10-21 20:54:48 +0200

    Created surveyRoutes

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
