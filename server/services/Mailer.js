fa03d6dc564caf249e7215c437958e8a00ffae63
Talovic <dzevad88@gmail.com>
2017-10-23 21:24:32 +0200

    Successfully created new survey instance

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -6,6 +6,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -37,6 +38,17 @@ class Mailer extends helper.Mail {
       peronalize.addTo(recipient);
     });
     this.addPersonalization(peronalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 
