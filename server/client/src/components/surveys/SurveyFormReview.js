e5dff7586f523b560f128b17401e71040661bddd
Talovic <dzevad88@gmail.com>
2017-11-04 17:05:18 +0100

    Now it is possible to review survey form input

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,23 @@
 //SurveyFormReview shows user their form inputs for review
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
