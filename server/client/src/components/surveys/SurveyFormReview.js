c63a8a0d35e4bd2eb4b74864a9c82618acde10d8
Talovic <dzevad88@gmail.com>
2017-11-05 21:22:23 +0100

    Added send survey button

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -18,8 +18,15 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <button
+        className="yellow darken-3 white-text btn-flat"
+        onClick={onCancel}
+      >
         Back
+      </button>
+      <button className="green btn-flat right white-text">
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
